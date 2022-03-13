4a2f2e9afb9c42e953a636e0cbd06d1869be6db5
Jorge <al385743@uji.es>
2022-03-13 21:30:29 +0100

    Vertex Buffer Layouts!

--- Cober/src/Cober/Application.cpp
+++ Cober/src/Cober/Application.cpp
@@ -6,6 +6,27 @@ namespace Cober {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type) 
+	{
+		switch (type)
+		{
+			case Cober::ShaderDataType::Float:		return GL_FLOAT;
+			case Cober::ShaderDataType::Float2:		return GL_FLOAT;
+			case Cober::ShaderDataType::Float3:		return GL_FLOAT;
+			case Cober::ShaderDataType::Float4:		return GL_FLOAT;
+			case Cober::ShaderDataType::Mat3:		return GL_FLOAT;
+			case Cober::ShaderDataType::Mat4:		return GL_FLOAT;
+			case Cober::ShaderDataType::Int:		return GL_INT;
+			case Cober::ShaderDataType::Int2:		return GL_INT;
+			case Cober::ShaderDataType::Int3:		return GL_INT;
+			case Cober::ShaderDataType::Int4:		return GL_INT;
+			case Cober::ShaderDataType::Bool:		return GL_BOOL;
+		}
+
+		SDL_LogInfo(0, "Uknown Shader Data Type!");
+		return 0;
+	}
+
 	Application::Application() {
 
 		s_Instance = this;
@@ -20,16 +41,34 @@ namespace Cober {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.33f, -0.5f, 1.0f,
-			 0.33f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.33f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.33f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,   0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout) {
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, 
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE, 
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -40,11 +79,15 @@ namespace Cober {
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main() 
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);	
 			}
 		)";
@@ -53,11 +96,14 @@ namespace Cober {
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
+
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main() 
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);	
+				color = v_Color;
 			}
 		)";
 
--- Cober/src/Cober/Layers/LayerStack.h
+++ Cober/src/Cober/Layers/LayerStack.h
@@ -20,7 +20,6 @@ namespace Cober {
 
 		std::vector<Layer*>::iterator begin() { return m_Layers.begin(); }
 		std::vector<Layer*>::iterator end() { return m_Layers.end(); }
-
 	private:
 		std::vector<Layer*> m_Layers;
 		unsigned int m_LayerInsertIndex = 0;
